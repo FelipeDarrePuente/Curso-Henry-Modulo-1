7bad27c9c2e22c221f051ed6674e791173969148
Agapitto <darre46@hotmail.com>
2022-04-28 10:18:46 -0300

    resuelto por companieros de StandUp e Instructor

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -1,15 +1,30 @@
 'use strict'
 
-function counter() {
+function counter() { // | Hecho por el instructor
   // Retorna una funcion que cuando sea invocada retorne un valor creciente.
   // el primer valor deberia ser 1.
   // Vas a tener que usar closures.
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  
+  var counter = 0
+
+  return function () {
+    counter++
+    return counter
+  }
+
 }
 
-function cacheFunction(cb) {
+// var newCounter = counter() // function() { ...}
+// console.log(newCounter())
+// console.log(newCounter())
+
+// var newCounter2 = counter()
+// console.log(newCounter2())
+
+function cacheFunction(cb) { // Hecho por los companieros de standup
   // Usa closures para crear un caché para la función cb.
   // la función que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
   // cuando la función que hayas retornado es invocada de nuevo, debería guardar el argumento y el resultado de la invocacion
@@ -17,13 +32,25 @@ function cacheFunction(cb) {
   // debería retornar el resultado (previamente guardado)
   // Ejemplo:
   // cb -> function(x) { return x * x; }
-  // si invocas la function que retornaste con 5, adentro deberia invocar cb(5) y retornar 25.
-  // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
+  // si invocas la function que retornaste con 5, adentro deberia invocar cb(5) y retornar 10.
+  // si la invocas de nuevo con 5, deberia retornar 10 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var cache = {}
+  return function(x) {
+    console.log(cache)
+    if (cache.hasOwnProperty(x)) {
+      return cache[x];
+    }
+    cache[x] = cb(x);
+    console.log(cache)
+    return cache[x];
+  }
 }
 
 // Bind
+
+// | Hecho por el instructor (todo lo de abajo)
 
 var instructor = {
   nombre: "Franco",
@@ -38,11 +65,13 @@ var alumno = {
 function getNombre(){
   return this.nombre;
 }
- // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
+// Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
+console.log(getNombreInstructor())
+console.log(getNombreAlumno())
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -61,12 +90,14 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(null, '*', '*');
+// console.log(textoAsteriscos('hola'))
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(null, '-', '-');
+// console.log(textoGuiones('hola'))
 
-let textoUnderscore = undefined;
-
+let textoUnderscore = crearCadena.bind(null, '_', '_');
+// console.log(textoUnderscore('hola'))
 
 
 // No modifiquen nada debajo de esta linea
